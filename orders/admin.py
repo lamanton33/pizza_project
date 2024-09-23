9512e1704e94c942f30b9c6e39bf1fdf660dc2d4
Poţeluev, Iurii (Stud. DACS) <i.poteluev@student.maastrichtuniversity.nl>
2024-09-23 12:57:07 +0200

    views

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+#Register your models here
 
-# Register your models here.
+from .models import (
+    Customer, Ingredient, Pizza, Drink, Dessert, 
+    Order, OrderItem, DeliveryPerson, PostalCode, DiscountCode
+)
+
+admin.site.register(Customer)
+admin.site.register(Ingredient)
+admin.site.register(Pizza)
+admin.site.register(Drink)
+admin.site.register(Dessert)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(DeliveryPerson)
+admin.site.register(PostalCode)
+admin.site.register(DiscountCode)
